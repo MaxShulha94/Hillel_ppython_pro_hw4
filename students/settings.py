d441cbab4a69d5ae7f1135b24e6f043f0ec9b480
MaxShulha94 <maxshulga94@gmail.com>
2023-09-27 22:57:15 +0300

    another commit

--- students/settings.py
+++ students/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    "teachers.apps.TeachersConfig",
     "create_student.apps.CreateStudentConfig",
     "django.contrib.admin",
     "django.contrib.auth",
